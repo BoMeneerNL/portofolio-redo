bbe28a04ee4b5d066d728713d384805ecbc28a18
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-02-12 22:01:37 +0100

    redo folders & files (2/2)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    /*enabled: true,*/
+    mode: 'layers',
+    content: ['./public/**/*.html'],
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
