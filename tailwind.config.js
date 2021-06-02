40e9f424e9d69ebcbda5917de22b5759542f73c3
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2021-06-03 00:28:53 +0200

    re-done file directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   purge: {
-    enabled: true,
-    mode: 'layers',
-    content: ['./public/**/*.html'],
+    enabled: false,
+    content: ['./scripts/phpstatic/*.php', './scripts/phpstatic/errors/*.php', './scripts/js/*.js', './*.php', './scripts/phpbg/*.php', './scripts/phpbg/sql-pdo/*.php'],
+    options: {
+      keyframes: true,
+    },
   },
-  darkMode: 'class',
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
